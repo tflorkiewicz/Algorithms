bb5539742858ac2e41eb579c7ee43a2e73f1374c
Tom Florkiewicz <tflorkiewicz@gmail.com>
2013-06-10 22:48:40 -0400

    commit of singly linked list

---
+++ LinkedList.Test/LinkyListTests.cs
@@ -0,0 +1,32 @@
+﻿using LinkyList.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkedList.Test
+{
+    [TestClass]
+    public class LinkyListTests
+    {
+        [TestMethod]
+        public void LinkyListNodeTests()
+        {
+            var tmp = new LinkyList<string>();
+            tmp.Add("element1");
+            tmp.Add("element2");
+            
+            Assert.AreEqual(2, tmp.Count);
+
+            tmp.Remove("element1");
+
+            Assert.AreEqual(1, tmp.Count);
+
+            tmp.Add("element3");
+
+            Assert.AreEqual(tmp.Count, 2);
+
+            tmp.Remove("element3");
+
+            
+
+        }
+    }
+}
